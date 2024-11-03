90450763b753b263f5142f2de064e8f66f0ddfe6
Yash Darbar <darbaryash44@gmail.com>
2024-11-03 22:58:00 +0530

    commit

--- app/actions/add-text.ts
+++ app/actions/add-text.ts
@@ -88,26 +88,3 @@ export async function callingGroq() {
 //         return { error: "ADD_TEXT_ERROR" };
 //     }
 // }
-// // "use server";
-
-// import { db } from "@/lib/db";
-// import { InputData } from "../type/input";
-
-// type Input = InputData;
-
-// export async function addText(values: Partial<InputData>) {
-//     try {
-//         if (!values.text) {
-//             return { error: "TEXT_REQUIRED" };
-//         }
-
-//         const text = await db.input.create({
-//             data: {
-//                 text: values.text,
-//             }
-//         })
-//     } catch (error) {
-//         console.log("[ADD_TEXT]", error);
-//         return { error: "ADD_TEXT_ERROR" };
-//     }
-// }
